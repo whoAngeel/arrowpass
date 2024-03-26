13e2134055b2f8f0a2df9b08b678f429122e7f6b
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-25 21:03:47 -0600

    Rutas de terminal

---
+++ apps/api/src/routes/terminal.routes.js
@@ -0,0 +1,50 @@
+const { Router } = require("express");
+
+const router = new Router();
+
+router.get("/", async (req, res, next) => {
+	try {
+		return;
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.get("/:id", async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		return id;
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.post("/", async (req, res, next) => {
+	try {
+		const body = req.body;
+		res.status(201).json(body);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.patch("/:id", async (req, res, next) => {
+	try {
+		const data = req.body;
+		const { id } = req.params;
+		res.status(200).json(data);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.delete("/:id", function (req, res, next) {
+	try {
+		const { id } = req.params;
+		res.status(200).json(id);
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
