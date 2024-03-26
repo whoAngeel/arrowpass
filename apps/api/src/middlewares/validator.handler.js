94ab9509cf2471d6363bb35eaedf7722b8d26c3f
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-26 00:04:06 -0600

    fix(validator.handler): corrigiendo el abortEarly para que me de todos los campos erroneos de una

--- apps/api/src/middlewares/validator.handler.js
+++ apps/api/src/middlewares/validator.handler.js
@@ -4,7 +4,7 @@ function validatorHandler(schema, property) {
 	return (req, res, next) => {
 		const data = req[property];
 		const { error } = schema.validate(data, {
-			aboutEarly: false,
+			abortEarly: false,
 		});
 		if (error) {
 			next(boom.badRequest(error));
