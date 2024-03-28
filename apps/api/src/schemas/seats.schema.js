a6e462eb490ab2f5b7ecff5e328800ff7b410ac8
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-03-27 19:06:44 -0600

    fix(seat.schema.js ticket.schema.js): change the schemas for the camelCase

--- apps/api/src/schemas/seats.schema.js
+++ apps/api/src/schemas/seats.schema.js
@@ -1,13 +1,13 @@
 const joi = require("joi");
 const id = joi.number().integer();
-const id_vehicle = joi.number().integer();
+const idVehicle = joi.number().integer();
 const status = joi.string(); //ocupado, desocupado
 const number = joi.number().positive();
 const position = joi.string(); // l:left c:center r:right
 
 const createSeatSchema = joi.object({
-	id_seat: id.required(),
-	id_vehicle: id_vehicle.required(),
+	idSeat: id.required(),
+	idVehicle: idVehicle.required(),
 	status,
 	number: number.required(),
 	position,
