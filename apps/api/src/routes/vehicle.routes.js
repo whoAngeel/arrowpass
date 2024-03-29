5d0cbe8459d2c1bbd928add6c6c7b1b27b465bb7
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-29 16:54:24 -0600

    feat(vehicle.routes): protegiendo las rutas de vehiculo con el jwt

--- apps/api/src/routes/vehicle.routes.js
+++ apps/api/src/routes/vehicle.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const passport = require("passport");
 const validatorHandler = require("../middlewares/validator.handler");
 const {
-  createVehicleSchema,
-  updateVehicleSchema,
-  getVehicleSchema,
+	createVehicleSchema,
+	updateVehicleSchema,
+	getVehicleSchema,
 } = require("../schemas/vehicle.schema");
 
 const VehicleService = require("../services/vehicle.service");
@@ -11,71 +12,79 @@ const service = new VehicleService();
 
 const router = new Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const vehicles = await service.findAll();
-    res.json(vehicles);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res, next) => {
+		try {
+			const vehicles = await service.findAll();
+			res.json(vehicles);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 router.get(
-  "/:id",
-  validatorHandler(getVehicleSchema, "params"),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const vehicle = await service.findOne(id);
-      return res.json(vehicle);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getVehicleSchema, "params"),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const vehicle = await service.findOne(id);
+			return res.json(vehicle);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.post(
-  "/",
-  validatorHandler(createVehicleSchema, "body"),
-  async (req, res, next) => {
-    try {
-      const data = req.body;
-      const newVehicle = await service.create(data);
-      res.status(201).json(newVehicle);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(createVehicleSchema, "body"),
+	async (req, res, next) => {
+		try {
+			const data = req.body;
+			const newVehicle = await service.create(data);
+			res.status(201).json(newVehicle);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.patch(
-  "/:id",
-  validatorHandler(getVehicleSchema, "params"),
-  validatorHandler(updateVehicleSchema, "body"),
-  async (req, res, next) => {
-    try {
-      const data = req.body;
-      const { id } = req.params;
-      const updateVehicle = await service.update(id, data);
-      res.json(updateVehicle);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getVehicleSchema, "params"),
+	validatorHandler(updateVehicleSchema, "body"),
+	async (req, res, next) => {
+		try {
+			const data = req.body;
+			const { id } = req.params;
+			const updateVehicle = await service.update(id, data);
+			res.json(updateVehicle);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.delete(
-  "/:id",
-  validatorHandler(getVehicleSchema, "params"),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const vehicle = await service.delete(id);
-      res.json(vehicle);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getVehicleSchema, "params"),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const vehicle = await service.delete(id);
+			res.json(vehicle);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 module.exports = router;
