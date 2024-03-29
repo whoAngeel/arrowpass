37a449b0ca46395beba31024b246fe02e582a99d
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-29 17:14:39 -0600

    feat(journey.routes): protegiendo las rutas de viajes con jwt

--- apps/api/src/routes/journey.routes.js
+++ apps/api/src/routes/journey.routes.js
@@ -5,124 +5,136 @@ const router = new Router();
 const JourneyService = require("../services/journey.service");
 const validatorHandler = require("../middlewares/validator.handler");
 const {
-  getJourneySchema,
-  createJourneySchema,
-  updateJourneySchema,
-  reservationJourneySchema,
+	getJourneySchema,
+	createJourneySchema,
+	updateJourneySchema,
+	reservationJourneySchema,
 } = require("../schemas/journey.schema");
+const passport = require("passport");
 const service = new JourneyService();
 
-router.get("/", async (req, res, next) => {
-  try {
-    let journeys = await service.findAll();
-    res.json(journeys);
-  } catch (error) {
-    next(error);
-  }
-});
-
 router.get(
-  "/:id",
-  validatorHandler(getJourneySchema, "params"),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const journey = await service.findOne(id);
-      return res.json(journey);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	async (req, res, next) => {
+		try {
+			let journeys = await service.findAll();
+			res.json(journeys);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.get(
-  "/:id/seats",
-  validatorHandler(getJourneySchema, "params"),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const journey = await service.getSeats(id);
-      return res.json(journey);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const journey = await service.findOne(id);
+			return res.json(journey);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.get(
-  "/:id/all",
-  validatorHandler(getJourneySchema, "params"),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const journey = await service.findOneWithRelations(id);
-      return res.json(journey);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id/seats",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const journey = await service.getSeats(id);
+			return res.json(journey);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
+
+router.get(
+	"/:id/all",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const journey = await service.findOneWithRelations(id);
+			return res.json(journey);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.post(
-  "/",
-  validatorHandler(createJourneySchema, "body"),
-  async (req, res, next) => {
-    try {
-      const body = req.body;
-      const newJourney = await service.create(body);
-      res.status(201).json(newJourney);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(createJourneySchema, "body"),
+	async (req, res, next) => {
+		try {
+			const body = req.body;
+			const newJourney = await service.create(body);
+			res.status(201).json(newJourney);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.post(
-  "/:id/reservation",
-  validatorHandler(getJourneySchema, "params"),
-  validatorHandler(reservationJourneySchema, "body"),
-  async (req, res, next) => {
-    try {
-      const data = req.body;
-      const { id } = req.params;
-      const reservation = await service.reservation(id, data);
+	"/:id/reservation",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	validatorHandler(reservationJourneySchema, "body"),
+	async (req, res, next) => {
+		try {
+			const data = req.body;
+			const { id } = req.params;
+			const reservation = await service.reservation(id, data);
 
-      res.json(reservation);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.json(reservation);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.patch(
-  "/:id",
-  validatorHandler(getJourneySchema, "params"),
-  validatorHandler(updateJourneySchema, "body"),
-  async (req, res, next) => {
-    try {
-      const data = req.body;
-      const { id } = req.params;
-      const updatedJourney = await service.update(id, data);
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	validatorHandler(updateJourneySchema, "body"),
+	async (req, res, next) => {
+		try {
+			const data = req.body;
+			const { id } = req.params;
+			const updatedJourney = await service.update(id, data);
 
-      res.json(updatedJourney);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.json(updatedJourney);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 router.delete(
-  "/:id",
-  validatorHandler(getJourneySchema, "params"),
-  async function (req, res, next) {
-    try {
-      const { id } = req.params;
-      const deletedJourney = await service.delete(id);
-      res.json(deletedJourney);
-    } catch (error) {
-      next(error);
-    }
-  }
+	"/:id",
+	passport.authenticate("jwt", { session: false }),
+	validatorHandler(getJourneySchema, "params"),
+	async function (req, res, next) {
+		try {
+			const { id } = req.params;
+			const deletedJourney = await service.delete(id);
+			res.json(deletedJourney);
+		} catch (error) {
+			next(error);
+		}
+	}
 );
 
 module.exports = router;
