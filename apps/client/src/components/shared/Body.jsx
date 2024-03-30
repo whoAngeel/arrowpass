2ec5b000bb0944113e01b139bf2b95a181b5e88c
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-03-30 03:19:48 -0600

    feat(sidebar.jsx): login and Register View

--- apps/client/src/components/shared/Body.jsx
+++ apps/client/src/components/shared/Body.jsx
@@ -7,7 +7,6 @@ const Body = ({contenido}) => {
     <div className="bg-white bg-opacity-[0.2] mx-32 w-[80%] mb-7 mt-20  rounded-xl phone:w-full phone:m-0 phone:rounded-none tablet:mx-5">
         {contenido == 'home' && <Buy/>}
         {contenido == 'tickets' && <Main/>}
-        
     </div>
   )
 }
