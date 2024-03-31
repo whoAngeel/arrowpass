08f43f7cc1ee2bea5e770d85c9f0c32df43a56a0
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-30 23:08:53 -0600

    feat(frontend.login): haciendo la peticion de login al backend y guardando el token en el storage
    aun falta redirigir a home

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import Login from './pages/Login';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div>
-		<Navbar/>
-		<BrowserRouter>
-		<Routes>
-			<Route path='/login' element={<Login/>}></Route>
-		</Routes>
-		</BrowserRouter>
-	</div>
-  );
+	return (
+		<div>
+			<Navbar />
+			{/* <BrowserRouter> */}
+			<Routes>
+				<Route path="/login" element={<Login />}></Route>
+			</Routes>
+			{/* </BrowserRouter> */}
+		</div>
+	);
 }
 
 export default App;
