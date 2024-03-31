1ebbc2774d3c5bddf4e3e58f82d10a3ebadbd18c
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-03-30 18:08:24 -0600

    refactor(app/client): creando un nuevo proyecto de frontend con daysi y chakra ui

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,14 +1,31 @@
-
-import { useState } from "react";
-import Body from "./components/shared/Body";
-import Sidebar from "./components/shared/Sidebar";
-
-export default function App() {
-  const[contenido,setContenido] = useState('home'); 
-  return (
-    <div className="flex h-screen">
-      <Sidebar cambiarContenido={setContenido}/>
-      <Body contenido={contenido}/>
-    </div>
-  );
+import { Button } from "antd";
+import { useToast } from "@chakra-ui/react";
+function App() {
+	return (
+		<>
+			<div className="bg-primary">Hola </div>
+			<Button type="primary">hola </Button>
+			<ToastExample></ToastExample>
+		</>
+	);
 }
+function ToastExample() {
+	const toast = useToast();
+	return (
+		<Button
+			onClick={() =>
+				toast({
+					title: "Account created.",
+					description: "We've created your account for you.",
+					status: "success",
+					duration: 9000,
+					isClosable: true,
+				})
+			}
+		>
+			Show Toast
+		</Button>
+	);
+}
+
+export default App;
--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,57 +1,28 @@
-import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa';
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		VitePWA({
-			registerType: 'autoUpdate',
-			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
 			manifest: {
-				name: 'Vite PWA Project',
-				short_name: 'Vite PWA Project',
-				theme_color: '#ffffff',
+				display: "standalone",
+				display_override: ["window-controls-overlay"],
+				lang: "es-MX",
+				name: "ArrowPass",
+				short_name: "AWP",
+				description: "ejemplo de pwa",
+				theme_color: "#f2f2f2",
 				icons: [
+					// TODO : agregar los tipos de iconos con nuestro logo
 					{
-						src: 'pwa-64x64.png',
-						sizes: '64x64',
-						type: 'image/png'
+						src: "vite.svg",
+						sizes: "64x64",
+						type: "svg",
 					},
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any'
-					},
-					{
-						src: 'maskable-icon-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'maskable'
-					}
 				],
 			},
-		})
+		}),
 	],
-	resolve: {
-		alias: {
-		  "@": path.resolve(__dirname, "./src"),
-		},
-	  },
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-				changeOrigin: true,
-			},
-		},
-	},
 });
