65490868bb2631f6c7f8e481bf914d7ed67cf4a4
Amado José <amadosanti546@gmail.com>
2024-04-04 05:59:44 -0600

    apple wallet endpoint

--- apps/api/src/routes/wallet.routes.js
+++ apps/api/src/routes/wallet.routes.js
@@ -13,46 +13,82 @@ const router = new Router();
  * 2.- Send by gmail
  */
 router.post("/generate", async (req, res, next) => {
-	// const service = new Demo();
-	console.log("Hola mundo");
+  // const service = new Demo();
+  console.log("Hola mundo");
 
-	try {
-		await service.createClass(req.body);
-		await service.createObject(req.body);
-		response = await service.createJwtNewObjects();
-		res.json({ message: "Class created successfully", button: response });
-	} catch (error) {
-		next(error);
-	}
+  try {
+    await service.createClass(req.body);
+    await service.createObject(req.body);
+    response = await service.createJwtNewObjects();
+    res.json({ message: "Class created successfully", button: response });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/send-google-ticket", async (req, res, next) => {
-	try {
-		await service.createClass(req.body);
-		await service.createObject(req.body);
-		const ticketBody = req.body;
-		const linkWToken = await service.createJwtNewObjects();
-		// const passenger = "angel";
-		const rta = await emailService.sendGMailTicket(ticketBody, linkWToken);
-		res.status(200).json(rta);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    await service.createClass(req.body);
+    await service.createObject(req.body);
+    const ticketBody = req.body;
+    const linkWToken = await service.createJwtNewObjects();
+    // const passenger = "angel";
+    const rta = await emailService.sendGMailTicket(ticketBody, linkWToken);
+    res.status(200).json(rta);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/send-apple-ticket", async (req, res, next) => {
-	try {
-		const { organization, ticket, passengerEmail } = req.body;
+  try {
+    const {
+      logos,
+      passenger,
+      seatNumber,
+      from,
+      to,
+      serviceNumber,
+      departureDate,
+      boardingGate,
+      billingToken,
+      folio,
+      category,
+      price,
+      payMethod,
+      status,
+      service,
+      email,
+    } = req.body;
 
-		const fileName = await generateAppleTicket(organization, ticket);
+    const organization = {
+      name: service,
+      headerLogo: logos.headerLogo,
+      footerLogo: logos.footerLogo,
+    };
 
-		const rta = await emailService.sendAppleTicket(
-			passengerEmail,
-			"./" + fileName
-		);
-		return res.status(200).json(rta);
-	} catch (error) {
-		next(error);
-	}
+    const ticket = {
+      passenger,
+      seatNumber,
+      from,
+      to,
+      serviceNumber,
+      departureDate,
+      boardingGate,
+      billingToken,
+      folio,
+      category,
+      price,
+      payMethod,
+      status,
+    };
+
+    const fileName = await generateAppleTicket(organization, ticket);
+
+    const rta = await emailService.sendAppleTicket(email, "./" + fileName);
+    return res.status(200).json(rta);
+  } catch (error) {
+    next(error);
+  }
 });
 module.exports = router;
